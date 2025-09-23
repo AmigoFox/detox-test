6ea45276cb5a1829d9a1d98175fac21b9e9fdc17
prost <prostoggwpandre@gmail.com>
2025-09-23 20:11:51 +0300

    базовая верстка и модальное окно

--- js/script.js
+++ js/script.js
@@ -2,19 +2,16 @@ const modal = document.getElementById('modal');
 const openBtns = document.querySelectorAll('.open-modal');
 const closeBtn = document.querySelector('.close');
 
-// Открыть модалку
 openBtns.forEach(function(btn) {
   btn.addEventListener('click', function() {
     modal.style.display = 'flex';
   });
 });
 
-// Закрыть модалку
 closeBtn.addEventListener('click', function() {
   modal.style.display = 'none';
 });
 
-// Закрытие по клику вне окна
 window.addEventListener('click', function(e) {
   if (e.target === modal) {
     modal.style.display = 'none';
